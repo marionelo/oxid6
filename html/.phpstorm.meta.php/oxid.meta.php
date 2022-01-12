f65bc3ce79a71184a1fba873f2512fe1ae12d205
Mario Sánchez <m.sanchez@cyberpuerta.mx>
2022-01-11 18:24:58 -0600

    Finxig error from database

---
+++ html/.phpstorm.meta.php/oxid.meta.php
@@ -0,0 +1,504 @@
+<?php
+/**
+ * Used by PhpStorm to map factory methods to classes for code completion, source code analysis, etc.
+ *
+ * The code is not ever actually executed and it only needed during development when coding with PhpStorm.
+ *
+ * @see https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Advanced+Metadata
+ */
+
+namespace PHPSTORM_META {
+    override(\oxNew(0), type(0));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    namespace OxidEsales\WysiwygModule\Application\Controller {
+    class TextEditorHandler_parent extends \OxidEsales\Eshop\Application\Controller\TextEditorHandler
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\WysiwygModule\Core {
+    class ViewConfig_parent extends \OxidEsales\Eshop\Core\ViewConfig
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\WysiwygModule\Core {
+    class Language_parent extends \OxidEsales\Eshop\Core\Language
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\WysiwygModule\Core {
+    class Utils_parent extends \OxidEsales\Eshop\Core\Utils
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    namespace OxidEsales\GdprOptinModule\Component\Widget {
+    class ArticleDetails_parent extends \OxidEsales\Eshop\Application\Component\Widget\ArticleDetails
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\GdprOptinModule\Component\Widget {
+    class Review_parent extends \OxidEsales\Eshop\Application\Component\Widget\Review
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\GdprOptinModule\Component {
+    class UserComponent_parent extends \OxidEsales\Eshop\Application\Component\UserComponent
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\GdprOptinModule\Controller {
+    class ReviewController_parent extends \OxidEsales\Eshop\Application\Controller\ReviewController
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\GdprOptinModule\Controller {
+    class ArticleDetailsController_parent extends \OxidEsales\Eshop\Application\Controller\ArticleDetailsController
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\GdprOptinModule\Controller {
+    class ContactController_parent extends \OxidEsales\Eshop\Application\Controller\ContactController
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Core {
+    class ViewConfig_parent extends \OxidEsales\Eshop\Core\ViewConfig
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Component {
+    class BasketComponent_parent extends \OxidEsales\Eshop\Application\Component\BasketComponent
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Component\Widget {
+    class ArticleDetails_parent extends \OxidEsales\Eshop\Application\Component\Widget\ArticleDetails
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Controller {
+    class OrderController_parent extends \OxidEsales\Eshop\Application\Controller\OrderController
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Controller {
+    class PaymentController_parent extends \OxidEsales\Eshop\Application\Controller\PaymentController
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Controller {
+    class WrappingController_parent extends \OxidEsales\Eshop\Application\Controller\WrappingController
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Controller\Admin {
+    class OrderList_parent extends \OxidEsales\Eshop\Application\Controller\Admin\OrderList
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Controller\Admin {
+    class DeliverySetMain_parent extends \OxidEsales\Eshop\Application\Controller\Admin\DeliverySetMain
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class Address_parent extends \OxidEsales\Eshop\Application\Model\Address
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class User_parent extends \OxidEsales\Eshop\Application\Model\User
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class Order_parent extends \OxidEsales\Eshop\Application\Model\Order
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class Basket_parent extends \OxidEsales\Eshop\Application\Model\Basket
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class Article_parent extends \OxidEsales\Eshop\Application\Model\Article
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class PaymentGateway_parent extends \OxidEsales\Eshop\Application\Model\PaymentGateway
+    {
+
+    }
+}
+
+
+    namespace OxidEsales\PayPalModule\Model {
+    class PaymentList_parent extends \OxidEsales\Eshop\Application\Model\PaymentList
+    {
+
+    }
+}
+
+
+    namespace OxidProfessionalServices\Usercentrics\Core {
+    class ScriptRenderer_parent extends \OxidEsales\Eshop\Core\ViewHelper\JavaScriptRenderer
+    {
+
+    }
+}
+
+
+    namespace OxidProfessionalServices\Usercentrics\Core {
+    class ViewConfig_parent extends \OxidEsales\Eshop\Core\ViewConfig
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaBasket_parent extends \OxidEsales\Eshop\Application\Model\Basket
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaUser_parent extends \OxidEsales\Eshop\Application\Model\User
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaArticle_parent extends \OxidEsales\Eshop\Application\Model\Article
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaOrder_parent extends \OxidEsales\Eshop\Application\Model\Order
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaAddress_parent extends \OxidEsales\Eshop\Application\Model\Address
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaPayment_parent extends \OxidEsales\Eshop\Application\Model\Payment
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaCountryList_parent extends \OxidEsales\Eshop\Application\Model\CountryList
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaOrderArticle_parent extends \OxidEsales\Eshop\Application\Model\OrderArticle
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class KlarnaUserPayment_parent extends \OxidEsales\Eshop\Application\Model\UserPayment
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller {
+    class KlarnaThankYouController_parent extends \OxidEsales\Eshop\Application\Controller\ThankYouController
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller {
+    class KlarnaViewConfig_parent extends \OxidEsales\Eshop\Core\ViewConfig
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller {
+    class KlarnaOrderController_parent extends \OxidEsales\Eshop\Application\Controller\OrderController
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller {
+    class KlarnaUserController_parent extends \OxidEsales\Eshop\Application\Controller\UserController
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller {
+    class KlarnaPaymentController_parent extends \OxidEsales\Eshop\Application\Controller\PaymentController
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller {
+    class KlarnaBasketController_parent extends \OxidEsales\Eshop\Application\Controller\BasketController
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller\Admin {
+    class KlarnaOrderAddress_parent extends \OxidEsales\Eshop\Application\Controller\Admin\OrderAddress
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller\Admin {
+    class KlarnaOrderList_parent extends \OxidEsales\Eshop\Application\Controller\Admin\OrderList
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller\Admin {
+    class KlarnaOrderArticle_parent extends \OxidEsales\Eshop\Application\Controller\Admin\OrderArticle
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller\Admin {
+    class KlarnaOrderMain_parent extends \OxidEsales\Eshop\Application\Controller\Admin\OrderMain
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller\Admin {
+    class KlarnaOrderOverview_parent extends \OxidEsales\Eshop\Application\Controller\Admin\OrderOverview
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Controller\Admin {
+    class KlarnaPaymentMain_parent extends \OxidEsales\Eshop\Application\Controller\Admin\PaymentMain
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Component {
+    class KlarnaBasketComponent_parent extends \OxidEsales\Eshop\Application\Component\BasketComponent
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Component {
+    class KlarnaUserComponent_parent extends \OxidEsales\Eshop\Application\Component\UserComponent
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Component\Widgets {
+    class KlarnaServiceMenu_parent extends \OxidEsales\Eshop\Application\Component\Widget\ServiceMenu
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Core {
+    class Config_parent extends \OxidEsales\Eshop\Core\Config
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Model {
+    class PaymentGateway_parent extends \OxidEsales\Eshop\Application\Model\PaymentGateway
+    {
+
+    }
+}
+
+
+    namespace TopConcepts\Klarna\Core {
+    class KlarnaShopControl_parent extends \OxidEsales\Eshop\Core\ShopControl
+    {
+
+    }
+}
+
